2809601d77e8630b2e1d4143972ac07b0abdc5b5
Kerby Keith Aquino <skepfoosky15@gmail.com>
2022-09-26 14:19:41 +0800

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center mt-20">
+			<h1 className="text-3xl">Next.js Tailwind CSS template</h1>
+			<span>Have fun lol</span>
+    </div>
+  )
+}
