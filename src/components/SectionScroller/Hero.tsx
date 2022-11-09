9bcebdb2cb0117ce2f2bdd756b2986bb67517b5b
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-11-09 15:38:36 +0800

    I overworked this crap for 7 hrs 54 mins

--- src/components/SectionScroller/Hero.tsx
+++ src/components/SectionScroller/Hero.tsx
@@ -1,17 +1,18 @@
-import { useEffect, useState } from "react"
 import Image from "next/image"
 import SectionScroller from "./SectionScroller"
 import useMediaQuery from "hooks/useMediaQuery"
+import { motion } from "framer-motion"
 
 export function Hero() {
   const artLayout = useMediaQuery("(min-width: 1280px)")
 
   return (
     <SectionScroller name="hero" className="h-[125vh] bg-blue-100">
-      <div className="absolute top-0 left-0 right-0 h-screen">
+      <div className="absolute top-0 left-0 right-0 h-screen grid place-items-center">
         A Hero component
+        <div className="relative z-20">TESTTS</div>
       </div>
-      <div className="fixed -bottom-[9rem] w-full flex flex-col justify-end h-[45%] z-30">
+      <motion.div className="fixed -bottom-[9rem] w-full flex flex-col justify-end h-[45%] z-30">
         <div id="art" className="relative h-full w-full">
           <Image
             src="/images/anb_hero-art-front.png"
@@ -19,8 +20,8 @@ export function Hero() {
             objectFit={artLayout ? "cover" : "contain"}
           />
         </div>
-      </div>
-      <div
+      </motion.div>
+      <motion.div
         className="fixed w-full flex flex-col justify-end h-[70%]"
         style={{ bottom: artLayout ? "1rem" : "-6rem" }}
       >
@@ -31,7 +32,7 @@ export function Hero() {
             objectFit="contain"
           />
         </div>
-      </div>
+      </motion.div>
     </SectionScroller>
   )
 }
